e4c712b55b2ccdd6a2602a517322241c236cd982
shresthasumesh13@gmail.com <shresthasumesh13@gmail.com>
2024-06-03 16:58:52 +0545

    Made changes as per Bipin dai, made banners for all other pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -35,7 +35,10 @@
 var prevBtnID = new String();
 prevBtnID = null;
 
-function expand_collapse(btnID) {
+function expand_collapse(liID) {
+  btnID = document
+    .getElementById(liID)
+    .firstElementChild.lastElementChild.getAttribute("id");
   // Collapse logic
   if (prevBtnID != null && prevBtnID != btnID) {
     console.log(`collapsed: ${prevBtnID} \nexpanded: ${btnID}`);
@@ -84,23 +87,34 @@ function burger() {
   let header = document.getElementsByTagName("header")[0];
   let headerLogo = document.getElementsByClassName("navbar__logo-wrapper")[0];
   if (burger_toggle == 0) {
-    console.log("clicked");
-    burgerMenu.style.display = "block";
-    header.style.backgroundColor = "#FFFFFF";
-    burgerBtn.children[1].style.display = "block";
-    burgerBtn.children[0].style.display = "none";
-    console.log(headerLogo);
-    headerLogo.firstElementChild.style.display = "none";
-    headerLogo.lastElementChild.style.display = "block";
+    // console.log("clicked");
+    // burgerMenu.style.display = "block";
+    // header.style.backgroundColor = "#FFFFFF";
+    // burgerBtn.children[1].style.display = "block";
+    // burgerBtn.children[0].style.display = "none";
+    // console.log(headerLogo);
+    // headerLogo.firstElementChild.style.display = "none";
+    // headerLogo.lastElementChild.style.display = "block";
+
+    header.setAttribute("class", "burger-on");
     burger_toggle = 1;
   } else if (burger_toggle == 1) {
-    console.log("unclicked");
-    burgerMenu.style.display = "none";
-    header.style.backgroundColor = "transparent";
-    burgerBtn.children[1].style.display = "none";
-    burgerBtn.children[0].style.display = "block";
-    headerLogo.firstElementChild.style.display = "block";
-    headerLogo.lastElementChild.style.display = "none";
+    // console.log("unclicked");
+    // burgerMenu.style.display = "none";
+    // header.style.backgroundColor = "transparent";
+    // burgerBtn.children[1].style.display = "none";
+    // burgerBtn.children[0].style.display = "block";
+    // headerLogo.firstElementChild.style.display = "block";
+    // headerLogo.lastElementChild.style.display = "none";
+
+    header.setAttribute("class", null);
     burger_toggle = 0;
   }
 }
+
+// Event listener for window resize
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 767 && burger_toggle == 1) {
+    burger();
+  }
+});
